b352ff5086146358aa66713de2e5f5836563f597
Lukasz <65483365+Lunczer93@users.noreply.github.com>
2021-09-17 14:40:40 +0200

    Delete 6.dSegment.sql

--- 6.dSegment.sql
+++
@@ -1,8 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [SEG_ID]
-      ,[Segment]
-  FROM [Project2].[dbo].[dSegment]
-
---1. Looking at data
-SELECT *
-FROM [Project2].[dbo].[dSegment]
